55d9862cbffa1f7221d87858ff3fbb98fbe122d2
MohammedAbdulJabbar23 <96623329+MohammedAbdulJabbar23@users.noreply.github.com>
2023-07-28 21:23:24 +0300

    Update getPopularMovies.js

--- utils/getPopularMovies.js
+++ utils/getPopularMovies.js
@@ -1,6 +1,6 @@
 const catchAsync = require('../utils/asyncHandle');
 fetch = require('node-fetch');
-const apiKey = '8a5dae0dd85e45f838e3082c70a5a3d5'; // api key 
+const apiKey = ""; //put your api key 
 
 module.exports = async () => {
     const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
